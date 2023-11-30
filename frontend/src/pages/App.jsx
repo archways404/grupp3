30a7deddbc8ead83a9ff9c213e6689eb337c4143
archways <107893165+archways404@users.noreply.github.com>
2023-11-30 14:36:07 +0100

    styling

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -10,6 +10,15 @@ function App() {
 		setTestText(event.target.value);
 	};
 
+	const contextClass = {
+		success: 'bg-green-700',
+		error: 'bg-red-700',
+		info: 'bg-gray-700',
+		warning: 'bg-orange-500',
+		default: 'bg-indigo-700',
+		dark: 'bg-white-600 font-gray-300',
+	};
+
 	const handleSubmit = async (e) => {
 		e.preventDefault(); // Prevent default form submission behavior
 		try {
@@ -25,13 +34,13 @@ function App() {
 				const testData = data.test;
 				console.log(data.test);
 				setMsgText(testData);
-				toast.success(`${testData}`, {
+				toast.success(`Response from backend: ${testData}`, {
 					position: toast.POSITION.TOP_CENTER,
 				});
 			}
 		} catch (err) {
 			console.log(err);
-			toast.error('ERROR!', {
+			toast.error(`Response from backend: \n ${err}`, {
 				position: toast.POSITION.TOP_CENTER,
 			});
 		}
@@ -39,23 +48,31 @@ function App() {
 
 	return (
 		<>
-			<ToastContainer />
-			<div className="flex justify-center items-center h-screen">
+			<div className="flex justify-center items-center h-screen bg-slate-700">
+				<ToastContainer
+					toastClassName={({ type }) =>
+						contextClass[type || 'dark'] +
+						' relative flex p-1 min-h-10 rounded-md justify-between overflow-hidden cursor-pointer'
+					}
+					bodyClassName={() => 'text-sm font-white font-med block p-3'}
+					position="bottom-left"
+					autoClose={3000}
+				/>
 				<form
 					onSubmit={handleSubmit}
 					className="flex flex-col items-center space-y-4">
-					<label className="text-white">Edit Information</label>
 					<input
-						className="w-64 h-12 px-4 rounded-md bg-gray-900 text-gray-200"
-						placeholder="text"
+						className="w-64 h-12 px-4 rounded-md bg-slate-800 text-gray-200"
+						placeholder="text -> backend"
 						type="text"
 						value={testText}
 						onChange={handleChangeText}
 					/>
+					<br></br>
 					<button
 						type="submit"
-						className="bg-green-500 text-white w-32 rounded-full">
-						Submit
+						className="bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded">
+						Send to backend
 					</button>
 				</form>
 			</div>
